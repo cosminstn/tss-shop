7e9a3078c525fe1f2342557a1c147d6159544a04
Cosmin Stoian <cosmin@bceng.tech>
2020-04-21 17:47:57 +0300

    Minor changes.

--- src/test/java/school/tss/shop/BaseTest.java
+++ src/test/java/school/tss/shop/BaseTest.java
@@ -2,6 +2,9 @@ package school.tss.shop;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * The base test. All Tests that need spring dependencies should extend this test.
+ */
 @SpringBootTest(properties = {"spring.shell.interactive.enabled=false"})
-public class BaseTest {
+public abstract class BaseTest {
 }
