2894b5792bf119b14fd26973dcf2d3e853144ebf
Andreea1998 <andreea.aldescu@my.fmi.unibuc.ro>
2020-04-26 15:08:18 +0300

    Little modification

--- src/test/java/school/tss/shop/persistence/dao/ItemDAOTest.java
+++ src/test/java/school/tss/shop/persistence/dao/ItemDAOTest.java
@@ -1,6 +1,5 @@
 package school.tss.shop.persistence.dao;
 
-import com.sun.xml.internal.rngom.parse.host.Base;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import school.tss.shop.BaseTest;
