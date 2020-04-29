4e4c8f72acbd76124303999e1f39ace8b40a8e9e
Raissa Cosa <raissa.cosa@my.fmi.unibuc.ro>
2020-04-29 21:48:30 +0300

    Added CartService Tests.

--- src/test/java/school/tss/shop/service/CartServiceTest.java
+++ src/test/java/school/tss/shop/service/CartServiceTest.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import school.tss.shop.BaseTest;
 import school.tss.shop.exceptions.cart.InvalidProductIdException;
+import school.tss.shop.exceptions.cart.InvalidQtyException;
+import school.tss.shop.exceptions.cart.QtyTooLargeException;
 
 import java.time.Duration;
 import java.time.Instant;
@@ -29,30 +31,57 @@ class CartServiceTest extends BaseTest {
 	@Tag("cart")
 	@DisplayName("Tests if addProduct works as expected for existing items.")
 	@Test
+	@Disabled
 	void addProduct() throws Exception {
 		cartService.addProduct(1, 1);
 	}
 
 	@DisplayName("Tests if addProduct fails for invalid item id, throwing InvalidProductIdException.")
 	@Test
+	@Disabled
 	void whenInvalidProductId_thenFailAddProduct() {
 		assertThrows(InvalidProductIdException.class, () -> cartService.addProduct(19999, 1));
 	}
 
-	@Tag("cart")
+	@DisplayName("Tests if addProduct fails for qty greater then 100, throwing QtyTooLargeException.")
 	@Test
-	void getCartItems() {
+	void whenQtyTooLarge_thenFailAddProduct(){
+		assertThrows(QtyTooLargeException.class, () -> cartService.addProduct(1,120));
+	}
+
+	@DisplayName("Tests if addProduct fails for qty less then 100, throwing InvalidQtyException.")
+	@Test
+	void whenQtyTooSmall_thenFailAddProduct(){
+		assertThrows(InvalidQtyException.class, () -> cartService.addProduct(1,-10));
 	}
 
 	@Tag("cart")
 	@Test
+	@Disabled
+	void getCartItems() throws Exception {
+			cartService.addProduct(1,1);
+			assertEquals(1,cartService.getCartItems().get(0).getFirst().getId());
+		//assertEquals("Random product", itemDAO.create(item).getName());
+
+	}
+
+	@Tag("cart")
+	@Test
+	@Disabled
 	void getCartTotalValue() throws Exception {
 		cartService.addProduct(1 ,1);
 		assertTrue(cartService.getCartTotalValue() > 0);
 	}
 
+	@Test
+	void getCartTotalValueWhenEmpty() throws Exception {
+
+		assertEquals(0, cartService.getCartTotalValue());
+	}
+
 	@Tag("cart")
 	@Test
+	@Disabled
 	void clearCart() throws Exception {
 		cartService.addProduct(1, 2);
 		cartService.clearCart();
